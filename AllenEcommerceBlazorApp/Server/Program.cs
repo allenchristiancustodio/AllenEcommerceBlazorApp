69fe4da63bfa8cdf1f89607127558d5499921be9
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-08 22:59:31 +0800

    commit
    swaggerUI

--- AllenEcommerceBlazorApp/Server/Program.cs
+++ AllenEcommerceBlazorApp/Server/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.ResponseCompression;
+﻿
+global using AllenEcommerceBlazorApp.Shared;
+
+using Microsoft.AspNetCore.ResponseCompression;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,6 +30,7 @@ else
     app.UseHsts();
 }
 
+app.UseSwagger();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
--- AllenEcommerceBlazorApp/Shared/Product.cs
+++ AllenEcommerceBlazorApp/Shared/Product.cs
@@ -3,9 +3,15 @@ namespace AllenEcommerceBlazorApp.Shared
 {
     public class Product
     {
-        public Product()
-        {
-        }
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
     }
 }
 
