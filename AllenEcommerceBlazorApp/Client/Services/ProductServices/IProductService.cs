94a0031dec2e2732c3d866f9932bbbc912d3cc3e
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-09 00:37:31 +0800

    Best Practices
    Generic Services/ Data

---
+++ AllenEcommerceBlazorApp/Client/Services/ProductServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AllenEcommerceBlazorApp.Shared;
+
+namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+
+        Task GetProducts();
+    }
+}
+
---
+++ AllenEcommerceBlazorApp/Client/Services/ProductServices/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if(result !=null && result.Data !=null)
+                Products = result.Data;
+        }
+    }
+}
+
