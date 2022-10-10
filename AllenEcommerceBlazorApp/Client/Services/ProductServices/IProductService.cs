7e76bfa88b01544c953bc8290f9ae7440a9ec950
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-11 00:13:43 +0800

    Pagination
    implements on the client side

--- AllenEcommerceBlazorApp/Client/Services/ProductServices/IProductService.cs
+++ AllenEcommerceBlazorApp/Client/Services/ProductServices/IProductService.cs
@@ -10,11 +10,17 @@ namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
 
         List<Product> Products { get; set; }
 
+        int CurrentPage { get; set; }
+
+        int PageCount { get; set; }
+
+        string LastSearchText { get; set; }
+
         Task GetProducts(string? categoryUrl = null);
 
         Task<ServiceResponse<Product>> GetProduct(int productId);
 
-        Task SearchProducts(string searchText);
+        Task SearchProducts(string searchText , int page);
 
         Task<List<string>> GetProductSearchSearchSuggestions(string searchText);
     }
--- AllenEcommerceBlazorApp/Client/Services/ProductServices/ProductService.cs
+++ AllenEcommerceBlazorApp/Client/Services/ProductServices/ProductService.cs
@@ -14,6 +14,12 @@ namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading products.....";
 
+        public int CurrentPage { get; set; } = 1;
+
+        public int PageCount { get; set; } = 0;
+
+        public string LastSearchText { get; set; } = string.Empty;
+
         public event Action ProductChanged;
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -28,9 +34,17 @@ namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
             var result = categoryUrl == null ?
                 await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product/featured") :
                  await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
-            if (result !=null && result.Data !=null)
+            if (result != null && result.Data != null)
                 Products = result.Data;
 
+            CurrentPage = 1;
+            PageCount = 0;
+
+
+            if (Products.Count == 0)
+            {
+                Message = "No products found";
+            }
             ProductChanged.Invoke();
         }
 
@@ -42,11 +56,15 @@ namespace AllenEcommerceBlazorApp.Client.Services.ProductServices
             return result.Data;
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText,int page)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
-            if(result != null && result.Data !=null )
-                Products = result.Data;
+            LastSearchText = searchText;
+            var result = await _http.GetFromJsonAsync<ServiceResponse<ProductSearchResultDTO>>($"api/product/search/{searchText}/{page}");
+            if(result != null && result.Data !=null) { 
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
             if (Products.Count == 0) Message = "No Products found.";
             ProductChanged?.Invoke();
         }
