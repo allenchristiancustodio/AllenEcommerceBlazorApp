d3d3f8c1fec2be3c216d914d0629240f52c99621
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-11 01:27:26 +0800

    Add to Cart
    Add to cart Server Side

---
+++ AllenEcommerceBlazorApp/Server/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AllenEcommerceBlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService )
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost("products")]
+        public async Task<ActionResult<ServiceResponse<List<CartProductResponseDTO>>>> GetCartProducts(List<CartItem> cartItems)
+        {
+            var result = await _cartService.GetCartProducts(cartItems);
+            return Ok(result);
+        }
+
+
+    }
+}
+
--- AllenEcommerceBlazorApp/Server/Services/CartService/CartService.cs
+++ AllenEcommerceBlazorApp/Server/Services/CartService/CartService.cs
@@ -4,14 +4,57 @@ namespace AllenEcommerceBlazorApp.Server.Services.CartService
 {
     public class CartService : ICartService
     {
-        public CartService()
-        {
+        private readonly DataContext _context;
 
+        public CartService(DataContext context)
+        {
+            _context = context;
         }
 
-        public Task<ServiceResponse<List<CartProductResponseDTO>>> GetCartProducts(List<CartItem> cartItems)
+        public async Task<ServiceResponse<List<CartProductResponseDTO>>> GetCartProducts(List<CartItem> cartItems)
         {
-            throw new NotImplementedException();
+            var result = new ServiceResponse<List<CartProductResponseDTO>>
+            {
+                Data = new List<CartProductResponseDTO>()
+             };
+
+            foreach (var item in cartItems)
+            {
+                var product = await _context.Products
+                    .Where(p => p.Id == item.ProductId)
+                    .FirstOrDefaultAsync();
+
+                if(product == null)
+                {
+                    continue;
+                }
+
+                var productVariant = await _context.ProductVariants
+                    .Where(v => v.ProductId == item.ProductId
+                    && v.ProductTypeId == item.ProductTypeId)
+                    .Include(v => v.ProductType)
+                    .FirstOrDefaultAsync();
+
+                if(productVariant == null)
+                {
+                    continue;
+                }
+
+                var cartProduct = new CartProductResponseDTO
+                {
+                    ProductId = product.Id,
+                    Title = product.Title,
+                    ImageUrl = product.ImageUrl,
+                    Price = productVariant.Price,
+                    ProductType = productVariant.ProductType.Name,
+                    ProductTypeId = productVariant.ProductTypeId
+                };
+
+                result.Data.Add(cartProduct);
+            }
+
+            return result;
+
         }
     }
 }
