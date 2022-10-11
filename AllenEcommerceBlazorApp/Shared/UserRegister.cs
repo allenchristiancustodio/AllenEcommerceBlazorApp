2e3f7091cf456cae3dbf7e294f6937888d4be2e2
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-11 22:16:28 +0800

    UserRegister Model
    adding the UserRegister Model and some validation.

---
+++ AllenEcommerceBlazorApp/Shared/UserRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AllenEcommerceBlazorApp.Shared
+{
+    public class UserRegister
+    {
+        [Required , EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength =6)]
+        public string Password { get; set; } = string.Empty;
+        [Compare("Password",ErrorMessage ="The Password do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }  
+
+
+}
+
