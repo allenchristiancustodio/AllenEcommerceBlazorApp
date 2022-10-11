82ada065174af1452970458581b1ad2d51cc364c
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-10-12 02:11:01 +0800

    AuthStatePrivider
    Implementing the Custom Auth State Provider

--- AllenEcommerceBlazorApp/Client/Program.cs
+++ AllenEcommerceBlazorApp/Client/Program.cs
@@ -4,6 +4,7 @@ global using AllenEcommerceBlazorApp.Client.Services.ProductServices;
 global using AllenEcommerceBlazorApp.Client.Services.CategoryService;
 global using AllenEcommerceBlazorApp.Client.Services.CartService;
 global using AllenEcommerceBlazorApp.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AllenEcommerceBlazorApp.Client;
@@ -19,6 +20,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
 
